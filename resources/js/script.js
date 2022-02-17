f64bb757cbbdf8ce951b350eb954b70475bc2930
NUR UDDIN <99638208+nuruddin538@users.noreply.github.com>
2022-02-17 11:39:19 +0600

    Paul-Lapkin-Template

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+// Mixitup
+var mixer=mixitup('.grid-portfolio-container');
+// Scrollify
+$(function(){
+    $.scrollify({
+        section : ".scrollify",
+        setHeights: false,
+        updateHash: false,
+    });
+});
+// Hamburger Menu
+function openMenu(){
+    document.getElementById('navbar').style.height="100%";
+}
+function closeMenu(){
+    document.getElementById('navbar').style.height="0%";
+}
+// Smooth scroll
+$(function(){
+    $('.menu-item a, .scroll-down').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+});
+
